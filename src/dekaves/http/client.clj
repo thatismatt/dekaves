6bada4133929cebc8412895782fe7e5816d6cb19
Matt Lee <thatismatt@gmail.com>
2021-08-13 14:11:35 +0100

    Rename caller -> node in http.client

--- src/dekaves/http/client.clj
+++ src/dekaves/http/client.clj
@@ -2,18 +2,18 @@
   (:require [clojure.edn :as edn]
             [clj-http.client :as client]))
 
-(defn request [caller body]
-  {:pre [(:url caller)]}
+(defn request [node body]
+  {:pre [(:url node)]}
   (let [response (try
-                   (client/post (:url caller)
-                                   ;; ???: should we be (binding [*print-length* false])?
-                                   {:body (prn-str body)
-                                    :headers {"content-type" "application/edn"}
-                                    :throw-exceptions false})
+                   (client/post (:url node)
+                                ;; ???: should we be (binding [*print-length* false])?
+                                {:body (prn-str body)
+                                 :headers {"content-type" "application/edn"}
+                                 :throw-exceptions false})
                    (catch Exception e
                      {:type (.getName (type e))
                       :message (ex-message e)
-                       ;; DEBUG: :exception e
+                      ;; DEBUG: :exception e
                       }))]
     (if (-> response :status (= 200))
       (some-> response :body edn/read-string)
