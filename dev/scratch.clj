79dfe2a2a17819a75ae222a9273a1dafdb0a32d6
Matt Lee <thatismatt@gmail.com>
2023-12-11 10:21:02 +0000

    Add scratch code to test multiple requests

--- dev/scratch.clj
+++ dev/scratch.clj
@@ -1,5 +1,7 @@
 (ns scratch
-  (:require [com.stuartsierra.component :as component]
+  (:require [clojure.string :as str]
+            [clojure.java.io :as io]
+            [com.stuartsierra.component :as component]
             [dekaves.core :as core]
             [dekaves.http.server :as server]
             [dekaves.http.client :as client]
@@ -7,6 +9,9 @@
             [dekaves.worker :as worker]
             [dekaves.status :as status])
   (:import [java.util.concurrent LinkedBlockingQueue TimeUnit]))
+
+(defn rand-str [s]
+  (str/join (repeatedly s #(rand-nth "abcdefghijklmnopqrstuvwxyz"))))
 
 (defn node-start-stop [node-atom options]
   (if (or (nil? @node-atom) (= (:status (status/status @node-atom)) :stopped))
@@ -24,7 +29,15 @@
       (range)
       ["kite" "hawk" "rook" "lark" "swan" "crow" "dove" "heron" "raven" "pidgeon" "sparrow" "jackdaw" "swallow" "falcon" "eagle" "vulture" "wagtail"])
 
-(map (comp :status status/status deref) nodes)
+(map (juxt (comp :port :http :options deref)
+           (comp :id :options deref)
+           (comp :status status/status deref)) nodes)
+
+;; only stop
+(map (fn [node]
+       (if (= :stopped (:status (status/status @node)))
+         :already-stopped
+         (node-start-stop node nil))) nodes)
 
 (let [ns (map (fn [node]
                 {:id  (-> @node :options :id)
@@ -89,6 +102,48 @@
 (client/request {:uri {:scheme "http" :host "localhost" :port 9091}}
                 {:command :unknown})
 
-(map (comp :store deref :state deref) nodes)
+
+(client/request {:uri {:scheme "http" :host "localhost" :port 9091}}
+                {:command :status})
+
+(def go (atom true))
+
+(swap! go not)
+
+(future
+  (reset! go true)
+  (while @go
+    (Thread/sleep 500)
+    (prn (apply + (map (comp count :store deref :state deref) nodes))))
+  (prn :done!))
+
+(let [ns "dekaves"]
+  (.setLevel (org.slf4j.LoggerFactory/getLogger ns) ch.qos.logback.classic.Level/WARN))
+
+(let [rs (map
+          (fn [c node]
+            (merge (client/request {:uri {:scheme "http" :host "localhost" :port (-> @node :options :http :port)}}
+                                   c)
+                   c))
+          (repeatedly 1000 #(do {:command :store
+                                 :key   (rand-str 5)
+                                 :value (rand-str 5)}))
+          (cycle nodes))]
+  (future (time (count rs)))
+  (def results (concat results rs)))
+
+(->> results (map :result) frequencies)
+
+(->> results (map :key) frequencies (map second) frequencies)
+
+;; reset all nodes stores
+(do
+  (mapv #(-> % deref :state (swap! assoc :store {})) nodes)
+  nil)
+
+(sort-by first
+         (map (juxt (comp :id :options deref) (comp count :store deref :state deref)) nodes))
+
+(->> results (mapcat #(->> % :results (map :node))) frequencies (sort-by first))
 
 )
